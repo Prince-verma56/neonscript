9e52d09f8ea64297735399c52664cae7b87a56d3
Prince Verma <pvcodingz96@gmail.com>
2026-03-25 00:33:40 +0530

    07 Projects

--- src/app/api/demo/error/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-    throw new Error("Server Error: Something went wrong on the server!");
-
-    return NextResponse.json({ message: "This should not be reached" });
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,15 @@
-"use client"
-import React from 'react'
-import { useQuery, useMutation } from 'convex/react'
-import { api } from "../../convex/_generated/api"
-import { Button } from '@/components/ui/button'
-import DemoPage from './demo/page'
-import { UserAvatar } from '@clerk/nextjs'
+"use client";
 
+import { ProjectsView } from "@/features/auth/components/projects/components/projects-view";
 
-function page() {
-
-  const projects = useQuery(api.projects.get)
-  const createProject = useMutation(api.projects.create)
-
-
-
-  // 1) CLient Error --> throws in the browser
-  const handleClientError = () => {
-    throw new Error("Client error : Something went wrong in the browser.!")
-  };
-
-  // 2) (API) Server Error --> throws in the server
-  const handleServerError = async () => {
-    await fetch("/api/demo/error", { method: "POST" });
-  };
-
-  // 3) Inggest Error --> throws in the inggest
-  const handleInngestError = async () => {
-    await fetch("/api/demo/inngest-error", { method: "POST" });
-  };
-
+const Home = () => {
   return (
-    <>
+    <div>
 
+      <ProjectsView />
 
-<div className='flex flex-col gap-2 p-4'>
-
-
-  
-
-
-<UserAvatar />
-<DemoPage />
-
-
-<div className='flex gap-2 justify-center '>
-<Button variant={"destructive"} onClick={handleClientError}>Client Error</Button>
-<Button variant={"destructive"} onClick={handleServerError}>Server Error</Button>
-<Button variant={"destructive"} onClick={handleInngestError}>Inngest Error</Button>
-</div>
-
-</div>
-
-    </>
+    </div>
   )
 }
 
-export default page
+export default Home
